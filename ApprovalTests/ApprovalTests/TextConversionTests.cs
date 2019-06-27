65a5652e98d620a16a224d5573d3013b6f603501
Andrew Withers <andreww@thomasdev.co.uk>
2019-06-27 10:53:41 +0100

    Add basic example

---
+++ ApprovalTests/ApprovalTests/TextConversionTests.cs
@@ -0,0 +1,32 @@
+﻿using GemBox.Document;
+using NUnit.Framework;
+
+namespace ApprovalTests
+{
+    [TestFixture]
+    public class TextConversionTests
+    {
+        // based on sample code @ https://www.gemboxsoftware.com/document/examples/c-sharp-read-pdf/305
+
+        [OneTimeSetUp]
+        public void BeforeAnyTest()
+        {
+            ComponentInfo.SetLicense("FREE-LIMITED-KEY");
+        }
+
+        [Test]
+        public void Approval()
+        {
+            var v2content = ReadPdfContent(@"C:\Dummy Docs\mock v2 report.pdf");
+            var v3content = ReadPdfContent(@"C:\Dummy Docs\mock v3 report.pdf");
+
+            Assert.AreEqual(v2content, v3content);
+        }
+
+        private string ReadPdfContent(string pdfPath)
+        {
+            DocumentModel document = DocumentModel.Load(pdfPath);
+            return document.Content.ToString();
+        }
+    }
+}
